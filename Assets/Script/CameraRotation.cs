32ef6e8f043232e70910f81fac5e527780185fd7
Natascha <>
2024-03-03 18:51:07 +0700

    2

--- Assets/Script/CameraRotation.cs
+++ Assets/Script/CameraRotation.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
+
  {  public float minAngle;
     public float maxAngle;
     public float RotationSpeed;
     public Transform CameraAxisTransform;
+ 
     void Start()
     {
         
     }
-
-    
+       
     void Update()
     {
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime* RotationSpeed * Input.GetAxis("Mouse X"), 0);
         var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        if (newAngleX > 180)
+            newAngleX -= 360;
+        
         newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
         CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
     }
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController _characterController;
+    private float _fallVelocity = 0;
+    public float gravity = 9.8f;
+    
+
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        _fallVelocity += gravity + Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+    }
+}
