8035780877e6fb8184b5e87de8b82d101d921100
Natascha <>
2024-03-03 17:24:58 +0700

    2

---
+++ Assets/Script/CameraRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+ {  public float minAngle;
+    public float maxAngle;
+    public float RotationSpeed;
+    public Transform CameraAxisTransform;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime* RotationSpeed * Input.GetAxis("Mouse X"), 0);
+        var newAngleX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse Y");
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+        CameraAxisTransform.localEulerAngles = new Vector3(newAngleX, 0, 0);
+    }
+}
