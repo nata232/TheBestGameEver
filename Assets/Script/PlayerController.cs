e47b7195b259cc01271974c04c3c1c3c4a993034
Natascha <>
2024-03-03 20:15:19 +0700

    3

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,18 +7,55 @@ public class PlayerController : MonoBehaviour
     private CharacterController _characterController;
     private float _fallVelocity = 0;
     public float gravity = 9.8f;
-    
+    public float jumpForce;
+    public float speed;
+    private Vector3 _moveVector;
+
 
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
     }
 
-    
+    void Update()
+    {
+        //Movement
+        _moveVector = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
+    }
     void FixedUpdate()
     {
-        _fallVelocity += gravity + Time.fixedDeltaTime;
+        //Movement
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+
+        //Fall and jump
+        _fallVelocity += gravity * Time.fixedDeltaTime;
         _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        //Stop fall if on the ground
+        if (_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
 
     }
 }
